0ec83d0cb8e7f75ff03e0175a6f775a96efbfd56
Ilan <ilantch@gmail.com>
2014-03-17 21:36:49 +0200

    arithmetic server! adding fibonacci

--- ArithmeticServer.js
+++ ArithmeticServer.js
@@ -8,10 +8,28 @@ var port = 9615;
 http.createServer(function (request, response) {
 
     var queryData = url.parse(request.url, true).query;
+    var pathname  = url.parse(request.url, true).pathname;
     console.log('query data is', queryData);
     response.writeHead(200, {'Content-Type': 'text/html'});
-    if (! (queryData.a && queryData.b)) {
-        response.end('<span style="color:red; font-size:30px;">Bad Input, pls provide a and b!</h1>');
+    // handle fibonaci request
+    if (queryData.num ) {
+        var ans;
+        if ('/fibonacci' != pathname) {
+            ans = 'num must be used with fibonacci pathname!';
+        }
+        else {
+            n = parseInt(queryData.num);
+            if (n < 0) {
+                ans = 'no fibonacci response for negative values, num =' + num;
+            }
+            else {
+                ans = fibonaci(n);
+            }
+        }
+        response.end(ans + ''); // convert ans to string
+    }
+    else if (! (queryData.a && queryData.b)) {
+        response.end('<span style="color:red; font-size:30px;">Bad Input, pls provide a and b or num!</h1>');
     }
     else {
         var a = queryData.a;
@@ -39,7 +57,7 @@ http.createServer(function (request, response) {
                 break;
             default:
                 // nothin special here
-                response.end('please use one of the following methods: add/subtract/multuply/divide');
+                response.end('please use one of the following methods: add/subtract/multuply/divide \n or fibonacci with variable num');
         } // EO switch
         console.log('returning', ans);
         response.end(ans + ''); // convert ans to string
@@ -47,3 +65,13 @@ http.createServer(function (request, response) {
 }).listen(port);
 
 console.log('running - listening on port ', port);
+
+
+function fibonaci(n) {
+    if ((1 == n) || (0 == n) )  {
+        return 1;
+    }
+    else {
+        return (fibonaci(n-1) + fibonaci(n-2));
+    }
+}

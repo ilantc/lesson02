05097fc9e895718eec2fcddfca14910078f08574
Ilan <ilantch@gmail.com>
2014-03-17 19:07:12 +0200

    adding polyn.js

---
+++ polyn.js
@@ -0,0 +1,16 @@
+/**
+ * Created by User on 3/17/14.
+ */
+function isPolyndrom(a) {
+    for(var i = 0, j =a.length-1 ; i < j ; i++,j--){
+       if (a.charAt(i) != a.charAt(j)) {
+           return false;
+       }
+    }
+    return true;
+}
+
+console.log('true =?', isPolyndrom('a'));
+console.log('true =?', isPolyndrom('aa'));
+console.log('true =?', isPolyndrom('abcba'));
+console.log('false =?', isPolyndrom('abb'));
